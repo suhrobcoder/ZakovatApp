90d54f96adac12e3230b1f66badc2f87517a5a0e
suhrobcoder <suhrobcoder@gmail.com>
2021-05-05 20:13:46 +0500

    plugins updated and models added

---
+++ app/src/main/java/uz/suhrob/zakovat/data/model/Admin.kt
@@ -0,0 +1,6 @@
+package uz.suhrob.zakovat.data.model
+
+data class Admin(
+    val login: String = "",
+    val password: String = "",
+)
---
+++ app/src/main/java/uz/suhrob/zakovat/data/model/Answer.kt
@@ -0,0 +1,7 @@
+package uz.suhrob.zakovat.data.model
+
+data class Answer(
+    val team: String = "",
+    val answer: String = "",
+    val isRight: Int = 0,
+)
